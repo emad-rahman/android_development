91fb877b7483c117fffaa4ea7694849cebc39dd6
Emad Rahman <emadpoke@gmail.com>
2017-01-30 16:05:14 -0800

    made my first button with a toast

--- emadrahman/app/src/main/java/com/example/emadrahman/myfirstapp/MainActivity.java
+++ emadrahman/app/src/main/java/com/example/emadrahman/myfirstapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.emadrahman.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("My Message", "This is a magic message!");
+                Toast.makeText(getApplicationContext(), "OMFG is this working? For real!?!?!!", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
