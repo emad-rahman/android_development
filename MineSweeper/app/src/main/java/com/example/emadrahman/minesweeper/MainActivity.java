e7cf4c672e465a84f126df28309d42cf6674a117
Emad Rahman <emadpoke@gmail.com>
2017-02-17 19:44:40 -0800

    created main grid and can hide image in background -- pretty cool

--- MineSweeper/app/src/main/java/com/example/emadrahman/minesweeper/MainActivity.java
+++ MineSweeper/app/src/main/java/com/example/emadrahman/minesweeper/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.emadrahman.minesweeper;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NUM_ROWS = 4;
+    private static final int NUM_COLS = 7;
+    private String TAG = "Orientation Demo";
+
+    Button[][] buttons = new Button[NUM_ROWS][NUM_COLS];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +28,93 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setSecondActivityBtn();
+        Log.e(TAG, "Running onCreate()!");
+
+
+        populateButtons();
+    }
+
+    private void populateButtons() {
+        TableLayout table = (TableLayout) findViewById(R.id.tableForButtons);
+        for(int row = 0; row < NUM_ROWS; row++){
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f
+            ));
+            table.addView(tableRow);
+            for (int col = 0; col < NUM_COLS; col++){
+                final int FINAL_ROW = row;
+                final int FINAL_COL = col;
+
+                Button btn = new Button(this);
+                btn.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f
+                ));
+
+//                btn.setText(row + ", " + col);
+                btn.setPadding(0, 0, 0, 0);
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        gridButtonClicked(FINAL_ROW, FINAL_COL);
+                    }
+                });
+
+                tableRow.addView(btn);
+                buttons[row][col] = btn;
+            }
+        }
+    }
+
+    private void gridButtonClicked(int row, int col) {
+        Toast.makeText(this, "Button Clicked: " + row + ", " + col, Toast.LENGTH_SHORT).show();
+        Button btn = buttons[row][col];
+
+        //lock button size
+        lockButtonSizes();
+
+        //does not scale image
+//        btn.setBackgroundResource(R.drawable.smile_glasses);
+
+        //scale image to btn
+        int newWidth = btn.getWidth();
+        int newHeight = btn.getHeight();
+        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.smile_glasses);
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
+
+        Resources resource = getResources();
+        btn.setBackground(new BitmapDrawable(resource, scaledBitmap));
+
+    }
+
+    private void lockButtonSizes() {
+        for (int row = 0; row < NUM_ROWS; row++){
+            for (int col = 0; col < NUM_COLS; col++){
+                Button btn = buttons[row][col];
+
+                int width = btn.getWidth();
+                btn.setMinWidth(width);
+                btn.setMaxWidth(width);
+
+                int height = btn.getHeight();
+                btn.setMinHeight(height);
+                btn.setMaxHeight(height);
+            }
+        }
     }
 
     public void setSecondActivityBtn(){
+
+
         Button btn = (Button) findViewById(R.id.secondActivityBtn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Clicked This Button", Toast.LENGTH_SHORT).show();
-
-//                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 Intent intent = SecondActivity.makeIntent(MainActivity.this);
                 startActivity(intent);
             }
